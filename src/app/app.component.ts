1baaf9b40f13e6edd11d4b4436b8f8b6341c8db7
Jurgis Megnis <jurgis@Jurgiss-MacBook-Pro.local>
2025-02-05 00:17:17 +0200

    Added the logic to fetch data and pass it in the CoffeeItem component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CoffeeItemComponent } from "./coffee-item/coffee-item.component";
+/* import { RouterOutlet } from '@angular/router'; */
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CoffeeItemComponent],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
