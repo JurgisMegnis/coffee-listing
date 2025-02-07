23dd01dc3df1db9e61577a14a64282164611118d
Jurgis Megnis <jurgis@Jurgiss-MacBook-Pro.local>
2025-02-08 00:49:22 +0200

    Added styling to the component and laid it out in the grid

--- src/app/coffee-item/coffee-item.component.ts
+++ src/app/coffee-item/coffee-item.component.ts
@@ -19,12 +19,12 @@ import { CoffeeListing } from '../coffee-listing';
       <div class="secondary-info">
         @if (coffeeListing.votes > 0) {
           <div class="rating">
-            <img src="../public/Star_fill.svg" alt="">
-            <h2>{{coffeeListing.rating}} <span>{{coffeeListing.votes}}</span></h2>
+            <img src="Star_fill.svg" alt="">
+            <h2>{{coffeeListing.rating}} <span>({{coffeeListing.votes}} votes)</span></h2>
           </div>
         } @else {
           <div class="no-rating">
-            <img src="../public/Star.svg" alt="">
+            <img src="Star.svg" alt="">
             <h2>No ratings</h2>
           </div>
         }
