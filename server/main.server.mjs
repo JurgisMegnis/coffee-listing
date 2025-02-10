66dd149332e0e286b98e29b4772f4e4898a09198
Jurgis Megnis <jurgis@Jurgiss-MacBook-Pro.local>
2025-02-10 23:28:47 +0200

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VDYS5Y3U.mjs";import"./chunk-FGRCDDW6.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-BJXANSH5.mjs";import"./chunk-NI7UK7ZS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
